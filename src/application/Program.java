f5231de3086a48a2cda85dc8d0388ae0b9936ba9
Amara Santos <yarasousasantos24@gmail.com>
2024-11-02 17:21:33 -0300

    seller git class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,20 @@
 package application;
 
+import java.util.Date;
+
 import models.entities.Department;
+import models.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1,"books");
-		System.out.println(obj);
+	
+		
+		Seller seller = new Seller(21,"Marta","bob@gmail.com", new Date(),3000.0,obj);
+		
+		System.out.println(seller);
 
 	}
 
