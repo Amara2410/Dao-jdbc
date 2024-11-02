1325f488abd23673554127b81d90274e71076eae
Amara Santos <yarasousasantos24@gmail.com>
2024-11-02 17:54:07 -0300

    SelerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import models.entities.Department;
 import models.entities.Seller;
 
@@ -11,7 +13,8 @@ public class Program {
 		
 		Department obj = new Department(1,"books");
 	
-		
+		SellerDao sellerDao= DaoFactory.createSellerDao();
+				
 		Seller seller = new Seller(21,"Marta","bob@gmail.com", new Date(),3000.0,obj);
 		
 		System.out.println(seller);
---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,29 @@
+package model.dao.impl;
+
+import model.dao.SellerDao;
+import models.entities.Department;
+import models.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDao{
+
+	@Override
+	public void insert(Seller obj) {
+
+	}
+
+	@Override
+	public void update(Seller obj) {
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		
+	}
+
+	@Override
+	public Department findById(Integer id) {
+		return null;
+	}
+
+}
